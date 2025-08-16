60cfc551b5fbc3d9d8b60befbb64d2603245f6f8
paras <parasdhiman17y@gmail.com>
2025-08-16 18:31:10 +0530

    tried asyn

--- core/consumers.py
+++ core/consumers.py
@@ -1,31 +1,53 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
+from .models import Message
 
-class MyWebsocket(WebsocketConsumer):
-  def connect(self):
-    self.accept()
-    from .models import Message
-    all_message = Message.objects.all()
-    text_messages = [mes.text for mes in all_message]
-    self.send(text_data=json.dumps({
-            'response': text_messages
+class MyWebsocket(AsyncWebsocketConsumer):
+    async def connect(self):
+        # Add this user to the "chat" group
+        await self.channel_layer.group_add("chat", self.channel_name)
+        await self.accept()
+
+        # Send all previous messages to the user who just connected
+        all_message = await self.get_all_messages()
+        await self.send(text_data=json.dumps({
+            'response': all_message
         }))
-  
 
-  def receive(self, text_data):
-    
-    from .models import Message
-    data = json.loads(text_data)
-    msg = data.get("message" ,'')
-    Message.objects.create(text = msg)
+    async def disconnect(self, close_code):
+        # Remove from group on disconnect
+        await self.channel_layer.group_discard("chat", self.channel_name)
 
-    all_message = Message.objects.all()
-    text_messages = [mes.text for mes in all_message]
-    self.send(text_data=json.dumps({
-            'response': text_messages
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        msg = data.get("message", '')
+
+        # Save to database
+        await self.save_message(msg)
+
+        # Send the message to everyone in the group
+        await self.channel_layer.group_send(
+            "chat",
+            {
+                "type": "chat_message",
+                "message": msg,
+            }
+        )
+
+    async def chat_message(self, event):
+        # When group sends a message, broadcast it to WebSocket
+        all_message = await self.get_all_messages()
+        await self.send(text_data=json.dumps({
+            'response': all_message
         }))
-    
-  def disconnect(self , code):
-    print("disconnected")
 
-       
+    # Helper: get all messages
+    @staticmethod
+    async def get_all_messages():
+        messages = Message.objects.all()
+        return [mes.text for mes in messages]
+
+    # Helper: save a message
+    @staticmethod
+    async def save_message(msg):
+        Message.objects.create(text=msg)
--- core/routing.py
+++ core/routing.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-  path('chat_ws/' , consumers.MyWebsocket.as_asgi()  )
+  re_path('chat_ws/' , consumers.MyWebsocket.as_asgi()  )
 ]
